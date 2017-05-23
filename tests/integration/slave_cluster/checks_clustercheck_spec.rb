21467f0d1208f9f6245f76d32d137574f4394726
Petr Michalec <epcim@apealive.net>
2017-05-24 00:01:55 +0200

    fix inspec test

--- tests/integration/slave_cluster/checks_clustercheck_spec.rb
+++ tests/integration/slave_cluster/checks_clustercheck_spec.rb
@@ -1,6 +1,6 @@
-describe file('/etc/xinetd.d/mysql_clustercheck_9200') do
+describe file('/etc/xinetd.d/mysql_clustercheck.conf') do
     it('should exist')
-    its('content') { should match /clustercheck password clustercheck 1 1/ }
+    its('content') { should match /clustercheck clustercheck password 1 1/ }
 end
 
 describe file('/usr/local/bin/mysql_clustercheck') do
