8113b4efb3ef0a2a25eb6f912b4c9a6566fbfede
System Administrator <root@MacBook-Pro-princess.local>
2017-09-08 13:01:22 -0400

    solve problem 'Access denied for user 'root'@'localhost' (using password: NO)'

--- galera/files/init_bootstrap.sh
+++ galera/files/init_bootstrap.sh
@@ -7,7 +7,7 @@ retries=0
 
 while [ $counter -gt 0 ]
 do
-  mysql -u root -e"quit"
+  mysql -u root -e"quit" || mysql -u {{ service.admin.user }} -p{{ service.admin.password }} -e"quit"
   if [[ $? -eq 0 ]]; then
     echo "Sucessfully connected to the MySQL service ($retries retries)."
     exit 0
